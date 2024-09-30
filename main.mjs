fa343c2a4f12e1cbde220d110dd2ee71a3c53997
Jadiael1 <jadiael@hotmail.com.br>
2024-09-30 11:59:22 -0300

    refactor(main): Simplify question and adjust function calls

--- main.mjs
+++ main.mjs
@@ -21,11 +21,7 @@ import createTikTokVideo from './utils/createTikTokVideo.mjs';
         //     Me surpreenda, e me responda em pt-BR
         // `;
 
-        const question = `Crie uma historia onde a mesma comece a primeira linha com uma descrição de no maximo 50 caracteres e no começo da linha deve ter Descrição: seguido da descrição, e nessa descrição deve ter um resumo da historia, e apos a descrição a historia nas proximas linhas, e a historia deve ter 3 minutos baseada em dicas voltadas a nutrição especificamente com HerbaLife, que envolve dicas sobre os produtos da herbalife, dicas de cuidado com o corpo usando os produtos da herbalife.
-            A historia deve ser de interesse de mais de 90% da população brasileira e do mundo.
-            Vou usar essa historia para postar videos em plataformas de Streaming então a historia deve ser interessante para que eu possa ganhar views.
-            Me surpreenda, e me responda em pt-BR
-        `;
+        const question = `Gere um texto de 3 minutos falando sobre os produtos da Herbalife e seus benefícios. A primeira linha deve seguir o seguinte padrão: Descrição: seguido de uma descrição do texto que tenha no máximo 50 caracteres, resumindo o conteúdo apresentado. me responda em pt-BR.`;
 
         const responseText = await getChatGPTResponse(question, openai, dir);
         console.log('Historia gerada com sucesso com ChatGPT');
@@ -39,11 +35,11 @@ import createTikTokVideo from './utils/createTikTokVideo.mjs';
         await textToSpeech(withoutDescription, config.texttospeakkey, config.texttospeakvoiceid, dir);
         console.log('Áudio gerado com sucesso.');
 
-        // await generateImages(description, 5, openai, dir);
-        await generateImagesP(`nutrition`, 5, config.pexelsapikey, dir);
+        await generateImages(`diet`, 5, openai, dir);
+        // await generateImagesP(`diet`, 5, config.pexelsapikey, dir);
         console.log('Imagens geradas e salvas com sucesso.');
 
-        createTikTokVideo('./');
+        createTikTokVideo('./', 'Testando');
     } catch (error) {
         console.error('Ocorreu um erro:', error);
     }
